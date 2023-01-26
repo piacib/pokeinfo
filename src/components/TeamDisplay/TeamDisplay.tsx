4b03d10a22ab79dd024d3a9bda107d89d0b1fc73
piacib <ben.piacitelli@gmail.com>
2023-01-25 20:40:19 -1000

    removed unused code add testing mode

--- src/components/TeamDisplay/TeamDisplay.tsx
+++ src/components/TeamDisplay/TeamDisplay.tsx
@@ -2,30 +2,43 @@ import React, { useEffect, useState } from "react";
 import { Button, ButtonDisplay } from "./TeamDisplay.style";
 import SpriteImage from "../SpriteImage";
 import { pokemonNameFilter } from "./TeamDisplay.functions";
-import { RoomIdProp } from "../../App";
-import { useTeams } from "./useTeams";
 import PokeDexScreen from "../PokeDexScreen/PokeDex";
 import PokemonDataDisplay from "../PokemonDataDisplay/PokemonDataDisplay";
-import { useParams } from "react-router-dom";
 import { useWebSocket } from "../../hooks/useWebsSocket";
+import {
+  isDevelopmentMode,
+  opponentTestTeam,
+  userTestTeam,
+} from "../../developmentMode";
 
 interface TeamProps {
   opponentsTeam: boolean;
+  teamtoDisplay?: "p1" | "p2";
   battleRoomId: string;
 }
 // fetches latest pokemon data from auto updating github dataset
 export const TeamDisplay = ({ opponentsTeam, battleRoomId }: TeamProps) => {
   // const [teams] = useTeams(battleRoomId ? battleRoomId : "");
   const [index, setIndex] = useState(0);
-  const [teams] = useWebSocket(battleRoomId);
-  const pokemon = teams[Number(opponentsTeam) ? "p1" : "p2"][index];
-
+  const [teams, setTeams] = useWebSocket(battleRoomId);
+  const teamKey = Number(opponentsTeam) ? "p1" : "p2";
+  const pokemon = teams[teamKey][index];
   console.log("battleRoomId", battleRoomId);
+  useEffect(() => {
+    if (battleRoomId === "testing") {
+      setTeams({
+        p1: userTestTeam,
+        p2: opponentTestTeam,
+      });
+    }
+    return () => {};
+  }, []);
+
   return (
     <>
       <PokeDexScreen>
         <ButtonDisplay>
-          {teams[Number(opponentsTeam) ? "p1" : "p2"]?.map((x, idx) => (
+          {teams[teamKey]?.map((x, idx) => (
             <Button
               key={pokemonNameFilter(x) + idx}
               onClick={() => setIndex(idx)}
