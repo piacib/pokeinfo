d7e5c7a8f6591f0cefc025f264f6f7e9d6db223d
piacib <ben.piacitelli@gmail.com>
2023-01-31 09:52:43 -1000

    added test message

--- src/developmentMode.ts
+++ src/developmentMode.ts
@@ -1,7 +1,14 @@
 export const isDevelopmentMode = process.env.NODE_ENV === "development";
 export const devPathname = "battle-gen9randombattle-1710900595";
 export const devRoomId = "room-" + devPathname;
-export const userTestTeam = ["Altaria", "Abomasnow", "Flapple", "Drifblim", "Gyarados", "Giratina"];
+export const userTestTeam = [
+  "Altaria",
+  "Abomasnow",
+  "Flapple",
+  "Drifblim",
+  "Gyarados",
+  "Giratina",
+];
 export const opponentTestTeam = [
   "Annihilape",
   "Carbink",
@@ -12,3 +19,6 @@ export const opponentTestTeam = [
 ];
 export const testAbilities = ["Cheek Pouch", "Intimidate", "Swift Swim"];
 export const testTeam: [string[], string[]] = [userTestTeam, opponentTestTeam];
+
+const ouGen9Message =
+  "|player|p2|Bentanamo10|1||teamsize|p1|6|teamsize|p2|6|gen|9|tier|[Gen 9] OU|rule|Sleep Clause Mod: Limit one foe put to sleep|rule|Species Clause: Limit one of each Pokémon|rule|OHKO Clause: OHKO moves are banned|rule|Evasion Items Clause: Evasion items are banned|rule|Evasion Moves Clause: Evasion moves are banned|rule|Endless Battle Clause: Forcing endless battles is banned|rule|HP Percentage Mod: HP is shown in percentages|clearpoke|poke|p1|Azumarill, M||poke|p1|Ting-Lu||poke|p1|Cinderace, M||poke|p1|Maushold-Four||poke|p1|Chien-Pao||poke|p1|Polteageist-Antique||poke|p2|Amoonguss, F||poke|p2|Clodsire, F||poke|p2|Azumarill, M||poke|p2|Toxapex, M||poke|p2|Dragapult, M||poke|p2|Breloom, F||teampreview";
