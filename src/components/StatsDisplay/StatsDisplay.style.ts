55080a496c34fbd2fff1a26d805f99dabe847ff4
piacib <ben.piacitelli@gmail.com>
2023-02-01 10:01:01 -1000

    styled table

--- src/components/StatsDisplay/StatsDisplay.style.ts
+++ src/components/StatsDisplay/StatsDisplay.style.ts
@@ -6,6 +6,9 @@ export const StatsContainer = styled.table`
   border: 1px solid #aaa;
   font-size: 1.2em;
   padding: 0.2rem;
+  background-color: #6890f0;
+  border-radius: 15px;
+  color: black;
   div {
     display: flex;
     flex-direction: row;
@@ -28,7 +31,7 @@ export const StatName = styled.div`
 `;
 export const StatValue = styled.div``;
 export const StatsHead = styled.thead`
-  background: green;
+  /* background: green; */
   * > h2 {
     margin: 0;
   }
@@ -42,8 +45,35 @@ interface StatEntry {
 export const Bar = styled.div<StatEntry>`
   height: 20px;
   width: ${(props) => `${(props.stat * 100) / 255}%`};
-  background: yellow;
+  /* background: yellow; */
 `;
-export const StatsTableRow = styled.tr`
+
+interface RowType {
+  type?: string;
+}
+interface obj {
+  [k: string]: { background: string; bar: string; barBorder: string };
+}
+const colorObj: obj = {
+  hp: { background: "#FF5959", bar: "#FF0000", barBorder: "#A60000" },
+  atk: { background: "#F5AC78", bar: "#F08030", barBorder: "#9C531F" },
+  def: { background: "#FAE078", bar: "#F8D030", barBorder: "#A1871F" },
+  spa: { background: "#9DB7F5", bar: "#6890F0", barBorder: "#445E9C" },
+  spd: { background: "#A7DB8D", bar: "#78C850", barBorder: "#4E8234" },
+  spe: { background: "#FA92B2", bar: "#F85888", barBorder: "#A13959" },
+};
+const colorObjConverter = (
+  str: string | undefined,
+  key: "background" | "bar" | "barBorder",
+) => {
+  if (!str || !colorObj[str]) return "";
+  return colorObj[str][key];
+};
+export const StatsTableRow = styled.tr<RowType>`
   background-color: red;
+  background-color: ${(props) => colorObjConverter(props.type, "background")};
+  ${Bar} {
+    background-color: ${(props) => colorObjConverter(props.type, "bar")};
+    border: 1px solid ${(props) => colorObjConverter(props.type, "barBorder")};
+  }
 `;
