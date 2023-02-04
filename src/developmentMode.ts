5c296cd6c5bbdbf7fde9bb8b4c7e6d27b7f880de
piacib <ben.piacitelli@gmail.com>
2023-02-04 10:03:50 -1000

    added jumpluff to test team

--- src/developmentMode.ts
+++ src/developmentMode.ts
@@ -3,7 +3,7 @@ export const devPathname = "battle-gen9randombattle-1710900595";
 export const devRoomId = "room-" + devPathname;
 
 export const userTestTeam = [
-  "Altaria",
+  "Jumpluff",
   "Abomasnow",
   "Flapple",
   "Drifblim",
