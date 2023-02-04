96c6063678f38cf911348376c2821da26d35c208
piacib <ben.piacitelli@gmail.com>
2023-02-04 10:03:35 -1000

    added type check for pokemon with no items

--- src/components/ItemsDisplay/ItemsDisplay.tsx
+++ src/components/ItemsDisplay/ItemsDisplay.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { ItemsContainer } from "./Items.style";
-import { PropertyBtn, HiddenPropertyText } from "../PokemonDataDisplay/DataDisplay.style";
+import {
+  PropertyBtn,
+  HiddenPropertyText,
+} from "../PokemonDataDisplay/DataDisplay.style";
 import { Dex } from "@pkmn/dex";
 import { dexSearchPrepper } from "../../functions";
 
@@ -12,10 +15,12 @@ const ItemsDisplay: React.FC<ItemsDisplayProps> = ({ items }) => {
   return (
     <ItemsContainer>
       <h3>Items:</h3>
-      {items.map((item) => (
+      {items?.map((item) => (
         <PropertyBtn key={item}>
           {item}
-          <HiddenPropertyText>{Items[dexSearchPrepper(item)].desc}</HiddenPropertyText>
+          <HiddenPropertyText>
+            {Items[dexSearchPrepper(item)].desc}
+          </HiddenPropertyText>
         </PropertyBtn>
       ))}
     </ItemsContainer>
