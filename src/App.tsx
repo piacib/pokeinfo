99edf9e12de739e517cf3fe9c460e3228a341e79
piacib <ben.piacitelli@gmail.com>
2023-02-08 15:04:22 -1000

    added enter new battle props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import { TypeWriterContainer } from "./TypeWriterContainer.style";
-import { AppDisplay, Button } from "./App.style";
+import { AppDisplay, BattleButton, Button } from "./App.style";
 import { TeamDisplay } from "./components/TeamDisplay/TeamDisplay";
 import Home from "./components/Home/Home";
+import { UrlForm } from "./components/Home/Home.style";
 
 export interface RoomIdProp {
   /** room-battle-${string}-${number} */
@@ -12,14 +13,32 @@ export interface RoomIdProp {
 const App: React.FC = () => {
   const [opponentsTeam, setOpponentsTeam] = useState<boolean>(true);
   const [battleRoomId, setBattleRoomId] = useState("");
+  const [isInExtension, setIsInExtension] = useState(false);
+  const [displayUrlInput, setDisplayUrlInput] = useState(false);
   useEffect(() => {
     if (window.location.search) {
-      const regMatch = window.location.search.match(/=(.*)/);
+      const regMatch = window.location.search.match(/\?battleId=(.*)/);
+      console.log(regMatch);
       if (regMatch) {
+        setIsInExtension(true);
         setBattleRoomId(regMatch[1]);
       }
     }
   });
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      url: { value: string };
+    };
+    const battleIndex = target.url.value.indexOf("battle");
+    if (battleIndex === -1) {
+      return;
+    }
+    const battleRoomId = target.url.value.slice(battleIndex);
+    setBattleRoomId(battleRoomId);
+    console.log(battleRoomId);
+  };
+
   return (
     <>
       {battleRoomId ? (
@@ -27,6 +46,18 @@ const App: React.FC = () => {
           <Button onClick={() => setOpponentsTeam(!opponentsTeam)}>
             Switch Team
           </Button>
+          {!isInExtension && (
+            <BattleButton onClick={() => setDisplayUrlInput(!displayUrlInput)}>
+              Enter new battle
+            </BattleButton>
+          )}
+          {displayUrlInput && (
+            <UrlForm onSubmit={(e) => handleSubmit(e)}>
+              <label htmlFor="url">Enter Url:</label>
+              <input type="text" id="url" name="url" />
+              <input type="submit" value="Submit" />
+            </UrlForm>
+          )}
           <AppDisplay>
             <TypeWriterContainer>
               <h1>Poke Info</h1>
