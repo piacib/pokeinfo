83e6e64591acda200c6a20de4bd2b782f9323ab1
piacib <ben.piacitelli@gmail.com>
2023-02-15 08:27:03 -1000

    added dark light mode toggle

---
+++ src/components/ModeToggle/ModeToggle.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import { setTheme } from "./theme";
+// to adjust toggle size set font-size
+const Toggle = () => {
+  const [togClass, setTogClass] = useState("dark");
+  let theme = localStorage.getItem("theme");
+  // check local storage for preferred theme
+  useEffect(() => {
+    if (localStorage.getItem("theme") === "theme-dark") {
+      setTogClass("dark");
+    } else if (localStorage.getItem("theme") === "theme-light") {
+      setTogClass("light");
+    }
+  }, [theme]);
+  // toggle themes
+  const handleChange = () => {
+    if (localStorage.getItem("theme") === "theme-dark") {
+      setTheme("theme-light");
+      setTogClass("light");
+    } else {
+      setTheme("theme-dark");
+      setTogClass("dark");
+    }
+  };
+  return (
+    <div className="container_toggle">
+      <input
+        type="checkbox"
+        id="toggle"
+        className="toggle-checkbox"
+        onChange={handleChange}
+        checked={togClass === "light" ? true : false}
+      />
+      <label htmlFor="toggle" className="toggle-label">
+        <span className="toggle-label-background"></span>
+      </label>
+    </div>
+  );
+};
+
+export default Toggle;
