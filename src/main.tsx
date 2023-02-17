b0f156f79a54ae522a88b694f4499bca716a5a27
piacib <ben.piacitelli@gmail.com>
2023-02-17 15:03:36 -0800

    removed theme imports

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { GlobalStyles } from "./GlobalStyles";
-import { ThemeProvider } from "styled-components";
-import { theme } from "./theme";
 import FontStyles from "./FontStyles";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
