74405bb7c5842a003db6d266fd59532bad3f541d
piacib <ben.piacitelli@gmail.com>
2023-02-19 05:45:53 -0800

    changed coloring to darkMode

--- src/GlobalStyles.ts
+++ src/GlobalStyles.ts
@@ -1,10 +1,10 @@
 import { createGlobalStyle } from "styled-components";
 import { isDevelopmentMode } from "./developmentMode";
-
 export const GlobalStyles = createGlobalStyle`
 html{
     font-size:${isDevelopmentMode ? "10px" : ""};
     background-color:#eef2f5;
+    background-color: ${(props) => props.theme.backgroundColor};
 }
 body {
     background-color:#eef2f5;
