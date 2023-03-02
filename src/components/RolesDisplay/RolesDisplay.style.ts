05eea8ba3fd204cb307973d132b9ded74ff18f19
piacib <ben.piacitelli@gmail.com>
2023-03-02 10:00:54 -0800

    set role btn styles

--- src/components/RolesDisplay/RolesDisplay.style.ts
+++ src/components/RolesDisplay/RolesDisplay.style.ts
@@ -1,6 +1,15 @@
 import styled from "styled-components";
 import { PropertyDisplay } from "../PokemonDataDisplay/DataDisplay.style";
-export const RolesBtn = styled.button``;
+export const RolesBtn = styled.button`
+  border: 2px solid ${(props) => props.theme.fontColor};
+  background-color: ${(props) => props.theme.backgroundColor};
+
+  color: ${(props) => props.theme.fontColor};
+  :hover {
+    border: 2px solid red;
+  }
+`;
 export const RolesContainer = styled(PropertyDisplay)`
   grid-column: 2;
+  justify-content: space-between;
 `;
