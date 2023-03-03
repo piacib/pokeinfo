e0d6de4dadff307566429f926144a95e003c2cff
piacib <ben.piacitelli@gmail.com>
2023-03-03 11:23:39 -0800

    started converting to styled components

---
+++ src/components/ModeToggle/ModeToggle.style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ToggleContainer = styled.div`
+  display: grid;
+  position: relative;
+  font-size: 16px;
+  place-items: center;
+  width: 6.25em;
+  height: 3.375em;
+`;
