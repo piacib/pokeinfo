0ea30b1d585f4dab69155d6a2f2be81230bbc93f
piacib <ben.piacitelli@gmail.com>
2023-03-06 12:42:44 -0800

    added activepkm track and teamDisplay states

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,24 @@ import OptionsMenu from "./components/OptionsMenu/OptionsMenu";
 import UrlSearch from "./components/UrlSearch/UrlSearch";
 import TeamDisplay from "./components/TeamDisplay/TeamDisplay";
 import { TypeWriterContainer } from "./TypeWriterContainer.style";
-import { AppDisplay, Button, Header, Spacer } from "./App.style";
+import {
+  AppDisplay,
+  Button,
+  Header,
+  ModeToggleContainer,
+  Spacer,
+} from "./App.style";
 import { ThemeProvider } from "styled-components";
 import { themeObjGenerator } from "./theme";
 import { GlobalStyles } from "./GlobalStyles";
 import { useLightMode } from "./hooks/useLightMode";
+import PokeTracker from "./components/PokeTracker/PokeTracker";
 
 const App: React.FC = () => {
-  const [opponentsTeam, setOpponentsTeam] = useState<boolean>(true);
+  const [teamToDisplay, setTeamToDisplay] = useState<"p1" | "p2">("p2");
   const [battleRoomId, setBattleRoomId] = useState("");
   const [isInExtension, setIsInExtension] = useState(false);
+  const [activePkmTrack, setActivePkmTrack] = useState(false);
   const previousBattleRoomId = useRef("");
   const [lightMode, setLightMode] = useLightMode();
   // useEffect(() => {
@@ -26,6 +34,7 @@ const App: React.FC = () => {
       // console.log("regmatch", regMatch);
       if (regMatch) {
         setIsInExtension(true);
+        setActivePkmTrack(true);
         setBattleRoomId(regMatch[1]);
       }
     }
@@ -44,7 +53,7 @@ const App: React.FC = () => {
     setBattleRoomId(battleRoomIdTemp);
     // console.log(battleRoomIdTemp);
   };
-  // console.log("battleRoomId", battleRoomId);
+  // console.log("teamToDisplay", teamToDisplay);
   return (
     <>
       <GlobalStyles theme={themeObjGenerator(lightMode)} />
@@ -53,36 +62,86 @@ const App: React.FC = () => {
           <>
             {isInExtension && <Spacer />}
             <Header>
-              {!isInExtension ? (
+              <>
+                <OptionsMenu>
+                  {isInExtension && <Spacer />}
+                  <ModeToggleContainer>
+                    <h3>{lightMode === "light" ? "Dark" : "Light"} Mode:</h3>
+                    <ModeToggle
+                      togClass={lightMode}
+                      setTogClass={setLightMode}
+                    />
+                  </ModeToggleContainer>
+                  <PokeTracker
+                    toggle={activePkmTrack}
+                    setToggle={setActivePkmTrack}
+                  />
+                  {!isInExtension && <UrlSearch handleSubmit={handleSubmit} />}
+                </OptionsMenu>
+                <Button
+                  onClick={() => {
+                    if (teamToDisplay === "p1") {
+                      setTeamToDisplay("p2");
+                    } else {
+                      setTeamToDisplay("p1");
+                    }
+                  }}
+                >
+                  Switch Team
+                </Button>
+              </>
+              {/* {!isInExtension ? (
                 <>
                   <OptionsMenu>
                     <ModeToggle
                       togClass={lightMode}
                       setTogClass={setLightMode}
                     />
+                    <PokeTracker
+                      toggle={activePkmTrack}
+                      setToggle={setActivePkmTrack}
+                    />
                     <UrlSearch handleSubmit={handleSubmit} />
                   </OptionsMenu>
-                  <Button onClick={() => setOpponentsTeam(!opponentsTeam)}>
+                  <Button
+                    onClick={() => {
+                      if (teamToDisplay === "p1") {
+                        setTeamToDisplay("p2");
+                      } else {
+                        setTeamToDisplay("p1");
+                      }
+                    }}
+                  >
                     Switch Team
                   </Button>
                 </>
               ) : (
                 <>
-                  <Button onClick={() => setOpponentsTeam(!opponentsTeam)}>
+                  <Button
+                    onClick={() => {
+                      if (teamToDisplay === "p1") {
+                        setTeamToDisplay("p2");
+                      }
+
+                      setTeamToDisplay("p1");
+                    }}
+                  >
                     Switch Team
                   </Button>
                   <ModeToggle togClass={lightMode} setTogClass={setLightMode} />
                 </>
-              )}
+              )} */}
             </Header>
             <AppDisplay>
               <TypeWriterContainer>
                 <h1>Poke Info</h1>
               </TypeWriterContainer>
               <TeamDisplay
-                opponentsTeam={opponentsTeam}
+                teamToDisplay={teamToDisplay}
                 battleRoomId={battleRoomId}
                 previousBattleRoomId={previousBattleRoomId.current}
+                activePkmTrack={activePkmTrack}
+                setActivePkmTrack={setActivePkmTrack}
               />
             </AppDisplay>
           </>
