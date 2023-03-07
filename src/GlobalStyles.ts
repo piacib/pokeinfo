7b0c9d614e09868c590aa1277c57c9a99e318cc7
piacib <ben.piacitelli@gmail.com>
2023-03-07 12:27:49 -0800

    removed mid width

--- src/GlobalStyles.ts
+++ src/GlobalStyles.ts
@@ -25,8 +25,6 @@ body {
     background-color: ${(props) => props.theme.backgroundColor};
     margin: 0;
   display: flex;
-  /* place-items: center; */
-  min-width: 320px;
   min-height: 100vh;
 }
 #root {
