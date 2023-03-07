55752feb0ac72f70867287ea77e0975652226e25
piacib <ben.piacitelli@gmail.com>
2023-03-07 12:22:32 -0800

    set color for alt text

--- src/components/TeamDisplay/TeamDisplay.style.ts
+++ src/components/TeamDisplay/TeamDisplay.style.ts
@@ -2,6 +2,11 @@ import styled from "styled-components";
 export const ButtonDisplay = styled.div`
   display: flex;
   justify-content: space-between;
+  @media (max-width: 300px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+  }
 `;
 export const Button = styled.button`
   width: inherit;
@@ -10,4 +15,5 @@ export const Button = styled.button`
   height: 40px;
   border-radius: 0;
   padding: 0;
+  color: ${(props) => props.theme.fontColor};
 `;
