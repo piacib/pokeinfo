f5f63ba2f653f2a82c86d340631cc90c7e8c4cd1
piacib <ben.piacitelli@gmail.com>
2023-03-08 09:49:15 -0800

    repositioned move properties for small screens

--- src/components/MovesDisplay/MovesDisplay.style.ts
+++ src/components/MovesDisplay/MovesDisplay.style.ts
@@ -11,28 +11,27 @@ import {
   TypeColoredComponent,
 } from "../../App.style";
 export const HiddenMoveText = styled(HiddenPropertyText)``;
-const mediaWidth = "230px";
+const mediaWidth = "400px";
 export const MoveBtn = styled(TypeColoredComponent)`
   position: relative;
   ${hoverDisplayCss}
   ${PillDesign}
-  @media (max-width: ${mediaWidth}) {
-    position: static;
-  }
+
   &:hover ${HiddenMoveText} {
-    bottom: 2.1rem;
+    bottom: 100%;
     right: 0;
-    min-width: 170px;
+    width: 11rem;
     padding: ${(props) => props.theme.padding.medium};
     font-size: 1.1rem;
     margin: 5px 0;
-    @media (max-width: ${mediaWidth}) {
-      min-width: auto;
-      /* right: auto; */
+    @media (max-width: ${(props) => props.theme.media.smallScreen}) {
+      left: 0;
+      max-width: 130px;
     }
   }
   p {
     margin: 0.3rem 0;
+    white-space: nowrap;
   }
   border: none;
 `;
@@ -43,7 +42,7 @@ export const MoveInfo = styled.ul`
   margin: 0;
 `;
 const cssMoveProperty = css`
-  white-space: nowrap;
+  /* white-space: nowrap; */
 `;
 export const MoveProperty = styled.li`
   ${cssMoveProperty}
@@ -54,6 +53,7 @@ export const MoveDescription = styled(MoveProperty)`
 export const MovesContainer = styled(PropertyDisplay)`
   display: flex;
   position: relative;
+  padding-right: 4rem;
 `;
 
 export const MoveType = styled(MoveProperty)`
