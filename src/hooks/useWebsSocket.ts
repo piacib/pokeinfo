d76592e683b1156fb485772c08629edea137e89f
piacib <ben.piacitelli@gmail.com>
2023-03-10 08:42:34 -0800

    added mac check and safari regex

--- src/hooks/useWebsSocket.ts
+++ src/hooks/useWebsSocket.ts
@@ -1,24 +1,21 @@
 import React, { useEffect, useReducer, useRef, useState } from "react";
 import { isRandomBattle } from "../functions";
 import {
+  getActivePokemon,
   getBattleType,
   getBuiltTeam,
+  getRandomBattleData,
   getSafariBuiltTeam,
   getSafariSwappedPkm,
   getSwappedPkm,
+  isMac,
 } from "./websocket.functions";
-type teamsType = { p1: string[]; p2: string[] };
-type dataType = {
-  teams: teamsType;
-  activePokemon: teamsType;
-  initialTeamRecieved: boolean;
-};
+export type teamsType = { p1: string[]; p2: string[] };
 const showdownWs = "wss://sim3.psim.us/showdown/websocket";
 type ReturnType = [
   [teamsType, React.Dispatch<React.SetStateAction<teamsType>>],
   teamsType,
 ];
-const isMac = false; //window.navigator.userAgent.includes("Mac");
 const useWebSocketConnection = (
   battleRoomId: string,
 ): [
@@ -81,52 +78,54 @@ export const useWebSocket = (
   // handles random battle messages
   useEffect(() => {
     const battleType = getBattleType(message);
-    if (battleType && !isRandomBattle(battleType)) {
+    if (!battleType || !isRandomBattle(battleType)) {
       return;
     }
     console.log(battleType, "is random");
-    const swapped = !isMac
-      ? getSwappedPkm(message)
-      : getSafariSwappedPkm(message);
-    if (swapped) {
-      const { p1, p2 } = swapped;
-      const temp: teamsType = activePokemon;
-
-      if (p1 && temp.p1[0] !== p1[0]) {
-        temp.p1 = p1;
-      }
-      if (p2 && temp.p2[0] !== p2[0]) {
-        temp.p2 = p2;
-      }
-      setActivePokemon(temp);
-    }
-    if (swapped) {
-      let newTeams = teams;
-      if (swapped.p1) {
-        swapped.p1.map((newPokemon) => {
-          if (!newTeams.p1.includes(newPokemon)) {
-            newTeams = { p1: [...newTeams.p1, newPokemon], p2: newTeams.p2 };
-          }
-        });
-      }
-      if (swapped.p2) {
-        swapped.p2.map((newPokemon) => {
-          if (!newTeams.p2.includes(newPokemon)) {
-            newTeams = { p2: [...newTeams.p2, newPokemon], p1: newTeams.p1 };
-          }
-        });
-      }
-      setTeams(newTeams);
-    }
+    const { activePokemon: tempActive, teams: tempTeams } = getRandomBattleData(
+      message,
+      activePokemon,
+      teams,
+    );
+    console.log({ message, tempTeams, activePokemon });
+    setTeams(tempTeams);
+    setActivePokemon(activePokemon);
+    // const swapped = !isMac
+    //   ? getSwappedPkm(message)
+    //   : getSafariSwappedPkm(message);
+    // const temp = getActivePokemon(message, activePokemon);
+    // if (temp) {
+    //   setActivePokemon(temp);
+    // }
+    // if (swapped) {
+    //   let newTeams = teams;
+    //   if (swapped.p1) {
+    //     swapped.p1.map((newPokemon) => {
+    //       if (!newTeams.p1.includes(newPokemon)) {
+    //         newTeams = { p1: [...newTeams.p1, newPokemon], p2: newTeams.p2 };
+    //       }
+    //     });
+    //   }
+    //   if (swapped.p2) {
+    //     swapped.p2.map((newPokemon) => {
+    //       if (!newTeams.p2.includes(newPokemon)) {
+    //         newTeams = { p2: [...newTeams.p2, newPokemon], p1: newTeams.p1 };
+    //       }
+    //     });
+    //   }
+    //   setTeams(newTeams);
+    // }
   }, [message]);
+
   // handles non random battle messages
   useEffect(() => {
     // console.log("checking built team", teams);
     const battleType = getBattleType(message);
-    if (battleType && isRandomBattle(battleType)) {
+    if (!battleType || isRandomBattle(battleType)) {
       return;
     }
-    // console.log(battleType, "is built");
+
+    console.log(battleType, "is built");
     // gets team from inital load and
     // sets itself to not enter loop again
     if (!teamRecieved) {
@@ -134,31 +133,20 @@ export const useWebSocket = (
         ? getBuiltTeam(message)
         : getSafariBuiltTeam(message);
       if (tempTeams && tempTeams.p1.length && tempTeams.p2.length) {
-        // console.log("setting built team", tempTeams);
         setTeams(tempTeams);
         setTeamRecieved(true);
       }
-      return;
     }
-    // const swapped = !isMac
-    //   ? getSwappedPkm(message)
-    //   : getSafariSwappedPkm(message);
-    const swapped = getSwappedPkm(message);
-    console.log("message", message);
-    console.log("getSafariSwappedPkm", getSafariSwappedPkm(message));
-    console.log("getSwappedPkm", getSwappedPkm(message));
+    const swapped = !isMac
+      ? getSwappedPkm(message)
+      : getSafariSwappedPkm(message);
+    console.log(getSwappedPkm(message), getSafariSwappedPkm(message));
+    console.log({ message, swapped });
     if (swapped) {
-      const { p1, p2 } = swapped;
-      const temp: teamsType = activePokemon;
-      console.log("swapped,temp", swapped, temp);
-      if (p1 && p1.length && temp.p1[0] !== p1[0]) {
-        temp.p1 = p1;
+      const temp = getActivePokemon(swapped, activePokemon);
+      if (temp) {
+        setActivePokemon(temp);
       }
-      if (p2 && p2.length && temp.p2[0] !== p2[0]) {
-        temp.p2 = p2;
-      }
-      console.log("setActivePokemon", temp);
-      setActivePokemon(temp);
     }
   }, [message]);
   return [[teams, setTeams], activePokemon];
