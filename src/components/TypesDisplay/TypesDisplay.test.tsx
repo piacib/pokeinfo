87a1ea548a355edf2a388efd05753f914c16e225
piacib <ben.piacitelli@gmail.com>
2023-03-13 07:28:31 -0700

    added testing

---
+++ src/components/TypesDisplay/TypesDisplay.test.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../../theme";
+import "@testing-library/jest-dom";
+import TypesDisplay from "./TypesDisplay";
+import { TypeName } from "../../types";
+const typesArr: TypeName[] = ["Normal", "Fighting"];
+test("Types arr appears", () => {
+  render(
+    <ThemeProvider theme={theme}>
+      <TypesDisplay types={typesArr} />
+    </ThemeProvider>,
+  );
+  typesArr.forEach((x) => {
+    const regExp = new RegExp(x, "i");
+    const el = screen.getByText(regExp);
+    expect(el).toBeInTheDocument();
+  });
+});
+
