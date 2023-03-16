a3dd181cb7593cc3a223b84eaeb6d0478daa4300
piacib <ben.piacitelli@gmail.com>
2023-03-16 11:20:44 -0700

    added UseSpectaorshook and PokeSearch component

--- src/App.tsx
+++ src/App.tsx
@@ -18,25 +18,33 @@ import { useLightMode } from "./hooks/useLightMode";
 import PokeTracker from "./components/PokeTracker/PokeTracker";
 import { LoadingScreen } from "./components/LoadingScreen";
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
+const PokeSearch = React.lazy(
+  () => import("./components/PokeSearch/PokeSearch"),
+);
 const TeamDisplay = React.lazy(
   () => import("./components/TeamDisplay/TeamDisplay"),
 );
 
+const testNoSpecUrl =
+  "https://play.pokemonshowdown.com/battle-gen9randombattle-1821599719-7j7njjdppksd6fbxqeqsxe2h8tnib35pw";
 const App: React.FC = () => {
   const [teamToDisplay, setTeamToDisplay] = useState<"p1" | "p2">("p2");
   const [battleRoomId, setBattleRoomId] = useState("");
   const [isInExtension, setIsInExtension] = useState(false);
+  const spectatorsAllowed = useSpectatorsAllowed(
+    new URLSearchParams(window.location.search),
+  );
   const [activePkmTrack, setActivePkmTrack] = useState(true);
+  // previousBattleRoomId -> tracks previous battle room so
+  // ws can clear info when new url is searched
   const previousBattleRoomId = useRef("");
   const [lightMode, setLightMode] = useLightMode();
-
+  const params = new URLSearchParams(window.location.search);
   useEffect(() => {
-    if (window.location.search) {
-      const regMatch = window.location.search.match(/\?battleId=(.*)&/);
-      if (regMatch) {
-        setIsInExtension(true);
-        setBattleRoomId(regMatch[1]);
-      }
+    const paramBattleId = params.get("battleId");
+    if (paramBattleId) {
+      setIsInExtension(true);
+      setBattleRoomId(paramBattleId);
     }
   });
   const handleSubmit = (e: React.SyntheticEvent) => {
@@ -96,13 +104,17 @@ const App: React.FC = () => {
               </TypeWriterContainer>
               <ErrorBoundary>
                 <Suspense fallback={<LoadingScreen />}>
-                  <TeamDisplay
-                    teamToDisplay={teamToDisplay}
-                    battleRoomId={battleRoomId}
-                    previousBattleRoomId={previousBattleRoomId.current}
-                    activePkmTrack={activePkmTrack}
-                    setActivePkmTrack={setActivePkmTrack}
-                  />
+                  {spectatorsAllowed ? (
+                    <TeamDisplay
+                      teamToDisplay={teamToDisplay}
+                      battleRoomId={battleRoomId}
+                      previousBattleRoomId={previousBattleRoomId.current}
+                      activePkmTrack={activePkmTrack}
+                      setActivePkmTrack={setActivePkmTrack}
+                    />
+                  ) : (
+                    <PokeSearch battleRoomId={battleRoomId} />
+                  )}
                 </Suspense>
               </ErrorBoundary>
             </AppDisplay>
@@ -116,3 +128,12 @@ const App: React.FC = () => {
 };
 
 export default App;
+const useSpectatorsAllowed = (params: URLSearchParams) => {
+  const [spectatorsAllowed, setSpectatorsAllowed] = useState(true);
+  useEffect(() => {
+    if (params.get("noSpectators")) {
+      setSpectatorsAllowed(false);
+    }
+  });
+  return spectatorsAllowed;
+};
