066f24289f1ba43f8dd807838215ed3274457ea4
piacib <ben.piacitelli@gmail.com>
2023-03-16 11:29:52 -0700

    added and styled form

--- src/components/PokeSearch/PokeSearch.tsx
+++ src/components/PokeSearch/PokeSearch.tsx
@@ -1,41 +1,83 @@
 import { Dex } from "@pkmn/dex";
 import React, { useState } from "react";
 import PokeDexScreen from "../PokeDexScreen/PokeDex";
+import PokemonDataDisplay from "../PokemonDataDisplay/PokemonDataDisplay";
 import {
+  InputContainer,
+  TextInput,
   SearchForm,
   SearchLabel,
-} from "../PokemonDataDisplay/DataDisplay.style";
-import PokemonDataDisplay from "../PokemonDataDisplay/PokemonDataDisplay";
-import { TextInput } from "../UrlSearch/UrlSearch.style";
-import { InputContainer } from "./PokeSearch.style";
+  Submit,
+  OptionsList,
+  ErrorText,
+} from "./PokeSearch.style";
 interface Props {
   battleRoomId: string;
 }
 enum ERRORS {
   INVALIDNAME,
 }
-interface Props {
+interface FormProps {
   handleSubmit: (e: React.SyntheticEvent) => void;
+  setPkmn: React.Dispatch<React.SetStateAction<string>>;
 }
-const Form = ({ handleSubmit }: Props) => (
-  <SearchForm
-    onSubmit={(e) => {
-      e.preventDefault();
-      handleSubmit(e);
-    }}
-  >
-    <SearchLabel htmlFor="pokemon_search">Enter Pokemon: </SearchLabel>
-    <InputContainer>
-      <TextInput type="text" id="pokemon_search" name="pokemon_search" />
-      <select>
-        <option value="Annihilape">Annihilape</option>
-        <option value="Carbink">Carbink</option>
-        <option value="Azumarill">Azumarill</option>
-      </select>
-      <input type="submit" value="Submit" />
-    </InputContainer>
-  </SearchForm>
-);
+const poke = "Annihilape";
+const Form = ({ handleSubmit, setPkmn }: FormProps) => {
+  const [text, setText] = useState("");
+  const [previousSearches, setPreviousSearches] = useState<string[]>([]);
+  const [selected, setSelected] = useState(false);
+  return (
+    <SearchForm
+      onSubmit={(e) => {
+        e.preventDefault();
+        const target = e.target as typeof e.target & {
+          pokemon_search: { value: string };
+        };
+        const pokemon = target.pokemon_search.value.trim();
+        if (pokemon && !previousSearches.includes(pokemon)) {
+          console.log(previousSearches, pokemon);
+          setPreviousSearches([pokemon, ...previousSearches]);
+        }
+        handleSubmit(e);
+      }}
+    >
+      {/* <SearchLabel htmlFor="pokemon_search">Enter Pokemon: </SearchLabel> */}
+      <InputContainer>
+        <TextInput
+          placeholder="Enter Pokemon:"
+          onSelect={() => {
+            if (previousSearches.length) {
+              setSelected(true);
+            }
+          }}
+          onBlur={() => {
+            setTimeout(() => {
+              setSelected(false);
+            }, 200);
+          }}
+          type="text"
+          id="pokemon_search"
+          name="pokemon_search"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        ></TextInput>
+        <Submit type="submit" value="Submit" disabled={!Boolean(text)} />
+        <OptionsList shouldDisplay={selected}>
+          {previousSearches.map((pokemon) => (
+            <li
+              onClick={(e) => {
+                setPkmn(pokemon);
+                setText(pokemon);
+              }}
+            >
+              {pokemon}
+            </li>
+          ))}
+        </OptionsList>
+      </InputContainer>
+    </SearchForm>
+  );
+};
 const PokeSearch = ({ battleRoomId }: Props) => {
   const [pkmn, setPkmn] = useState("");
   const [error, setError] = useState<false | ERRORS>(false);
@@ -57,18 +99,20 @@ const PokeSearch = ({ battleRoomId }: Props) => {
   return (
     <>
       <PokeDexScreen>
-        <Form handleSubmit={handleSubmit} />
+        <Form handleSubmit={handleSubmit} setPkmn={setPkmn} />
       </PokeDexScreen>
       {!pkmn && (
         <>
-          <h3>
+          <ErrorText>
             This battle does not allow Spectators please search the pokemon you
             wish to display
-          </h3>
+          </ErrorText>
         </>
       )}
       {error === ERRORS.INVALIDNAME ? (
-        <p>It appears the searched pokemon {pkmn} is not in our database.</p>
+        <ErrorText>
+          It appears the searched pokemon {pkmn} is not in our database.
+        </ErrorText>
       ) : (
         <>
           {battleTypeRegex && pkmn && (
