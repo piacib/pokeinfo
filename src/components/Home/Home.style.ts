47d829625dc2d38c6fbacd5f9d49e8a59a4ab058
piacib <ben.piacitelli@gmail.com>
2023-03-20 07:17:25 -0700

    added larger font sizes for lg display

--- src/components/Home/Home.style.ts
+++ src/components/Home/Home.style.ts
@@ -21,9 +21,27 @@ export const ButtonContainer = styled.div`
 `;
 export const WelcomeText = styled.p`
   text-align: center;
+  font-size: 1.3rem;
+  padding: 0 1rem;
+  @media (max-width: 24rem) {
+    /* font-size: .8rem; */
+  }
 `;
 export const WelcomeHeader = styled.h1`
-  font-size: 4.5rem;
+  font-size: 10rem;
+  /* font-size: 4.5rem; */
+  @media (max-width: 82rem) {
+    font-size: 8rem;
+  }
+  @media (max-width: 70rem) {
+    font-size: 7rem;
+  }
+  @media (max-width: 60rem) {
+    font-size: 6rem;
+  }
+  @media (max-width: 50rem) {
+    font-size: 4.3rem;
+  }
   @media (max-width: 35rem) {
     font-size: 3rem;
   }
@@ -31,4 +49,3 @@ export const WelcomeHeader = styled.h1`
     font-size: 2rem;
   }
 `;
-
