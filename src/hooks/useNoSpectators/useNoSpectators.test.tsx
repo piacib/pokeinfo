5d956a3b1a84937b4a4f9c336d70624ebc0717a9
piacib <ben.piacitelli@gmail.com>
2023-03-28 18:26:30 -0700

    changed tests to use renderHook

--- src/hooks/useNoSpectators/useNoSpectators.test.tsx
+++ src/hooks/useNoSpectators/useNoSpectators.test.tsx
@@ -1,36 +1,35 @@
-import UseNoSpectatorsExample, {
-  exampleUrl,
-  CorrectTeam,
-} from "./UseNoSpectators.example";
 import "@testing-library/jest-dom";
 
-import { fireEvent, render, screen } from "@testing-library/react";
-
-test("UseNoSpectators reads user team", () => {
-  render(
-    <>
-      <UseNoSpectatorsExample />
-    </>,
-  );
-  const p1 = screen.getByText(/p1/i);
-  expect(p1.children.length).toBe(CorrectTeam.p1.length);
-  CorrectTeam.p1.forEach((pokemon) => {
-    const regEx = new RegExp(pokemon, "i");
-    const pkmn = screen.getByText(regEx);
-    expect(pkmn).toBeInTheDocument();
+import { fireEvent, render, renderHook, screen } from "@testing-library/react";
+import useNoSpectator from "./useNoSpectator";
+const CorrectTeam = {
+  p1: [
+    "Cacturne",
+    "Polteageist",
+    "Gallade",
+    "Crabominable",
+    "Hypno",
+    "Goodra-Hisui",
+  ],
+  p2: ["Quaquaval", "Grumpig", "Dunsparce", "Oricorio-Pa'u", "Garchomp"],
+};
+const exampleUrl = new URL(
+  "https://piacib.github.io/pokeinfo/?battleId=battle-gen9randombattle-1831739535&isExtension=true&userTeam=Cacturne%2CPolteageist%2CGallade%2CCrabominable%2CHypno%2CGoodra-Hisui&opponentsTeam=Quaquaval%2CGrumpig%2CDunsparce%2COricorio-Pa%27u%2CGarchomp",
+);
+const params = exampleUrl.searchParams;
+test("useNoSpectator gets user team data", () => {
+  const { result } = renderHook(() => useNoSpectator(params));
+  console.log("results", result.current);
+  const p1 = result.current[1].p1;
+  p1.forEach((x, idx) => {
+    expect(x).toBe(CorrectTeam.p1[idx]);
   });
 });
-test("UseNoSpectators reads opponents team", () => {
-  render(
-    <>
-      <UseNoSpectatorsExample />
-    </>,
-  );
-  const p1 = screen.getByText(/p2/i);
-  expect(p1.children.length).toBe(CorrectTeam.p2.length);
-  CorrectTeam.p2.forEach((pokemon) => {
-    const regEx = new RegExp(pokemon, "i");
-    const pkmn = screen.getByText(regEx);
-    expect(pkmn).toBeInTheDocument();
+test("useNoSpectator gets opponents team data", () => {
+  const { result } = renderHook(() => useNoSpectator(params));
+  console.log("results", result.current);
+  const p2 = result.current[1].p2;
+  p2.forEach((x, idx) => {
+    expect(x).toBe(CorrectTeam.p2[idx]);
   });
 });
