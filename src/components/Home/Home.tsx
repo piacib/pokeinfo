7cdf74376dbd076fb9f5837a11ca134341d2b495
piacib <ben.piacitelli@gmail.com>
2023-05-09 16:24:38 -0700

    updated home links

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,31 +1,15 @@
-import React, { useState } from "react";
-import { devRoomId } from "../../developmentMode";
+import React from "react";
 import { TypeWriterContainer } from "../../TypeWriterContainer.style";
-import { Form } from "../UrlSearch/UrlSearch";
-import { UrlForm } from "../UrlSearch/UrlSearch.style";
 import {
   ButtonContainer,
   HomeContainer,
   WelcomeHeader,
   WelcomeText,
 } from "./Home.style";
-interface Props {
-  setBattleRoomId: React.Dispatch<React.SetStateAction<string>>;
-}
-const Home: React.FC<Props> = ({ setBattleRoomId }) => {
-  const [displayUrlInput, setDisplayUrlInput] = useState(false);
-  const handleSubmit = (e: React.SyntheticEvent) => {
-    e.preventDefault();
-    const target = e.target as typeof e.target & {
-      url: { value: string };
-    };
-    const battleIndex = target.url.value.indexOf("battle");
-    if (battleIndex === -1) {
-      return;
-    }
-    const battleRoomId = target.url.value.slice(battleIndex);
-    setBattleRoomId(battleRoomId);
-  };
+import { Link } from "react-router-dom";
+import { examplePath, inBattlePath, quizPath } from "../../router/routes";
+
+const Home: React.FC = ({}) => {
   return (
     <HomeContainer>
       <TypeWriterContainer>
@@ -36,12 +20,10 @@ const Home: React.FC<Props> = ({ setBattleRoomId }) => {
         click example
       </WelcomeText>
       <ButtonContainer>
-        <button onClick={() => setBattleRoomId(devRoomId)}>Example</button>
-        <button onClick={() => setDisplayUrlInput(!displayUrlInput)}>
-          Url
-        </button>
+        <Link to={inBattlePath}>Search A battle</Link>
+        <Link to={examplePath}>Example</Link>
+        <Link to={quizPath}>Effectiveness Quiz</Link>
       </ButtonContainer>
-      {displayUrlInput && <Form handleSubmit={handleSubmit} />}
     </HomeContainer>
   );
 };
