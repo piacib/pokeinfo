bc96e105d35755e3a093d2025dda4e5a055109fb
piacib <ben.piacitelli@gmail.com>
2023-05-09 16:31:44 -0700

    added paths obj

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,3 +5,11 @@ export const inBattlePath = "/inBattle/:id";
 export const quizPath = "/quiz";
 export const homePath = "/home";
 export const examplePath = `/inBattle/${devRoomId}`;
+
+export const paths = {
+  effectiveness: "/effectiveness",
+  inBattle: "/inBattle/:id",
+  quiz: "/quiz",
+  home: "/home",
+  example: `/inBattle/${devRoomId}`,
+};
