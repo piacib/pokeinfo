9132bbfd2a856da10b4521a21d312ccb8bfada0c
piacib <ben.piacitelli@gmail.com>
2023-05-09 16:25:29 -0700

    added routes to seperate file

---
+++ src/router/routes.ts
@@ -0,0 +1,7 @@
+import { devRoomId } from "../developmentMode";
+
+export const effectivenessPath = "/effectiveness";
+export const inBattlePath = "/inBattle/:id";
+export const quizPath = "/quiz";
+export const homePath = "/home";
+export const examplePath = `/inBattle/${devRoomId}`;
