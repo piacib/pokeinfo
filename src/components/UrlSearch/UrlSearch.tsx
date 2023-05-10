cccc331362e50b1b864f27b5b120d2a538a1a79f
piacib <ben.piacitelli@gmail.com>
2023-05-10 14:12:39 -0700

    added new search form

--- src/components/UrlSearch/UrlSearch.tsx
+++ src/components/UrlSearch/UrlSearch.tsx
@@ -1,16 +1,48 @@
-import { SubmitInput, TextInput, UrlForm, UrlLabel } from "./UrlSearch.style";
+import {
+  Button,
+  ButtonContainer,
+  InputLabel,
+  InputLabelSpan,
+  TextInputField,
+  UrlForm,
+} from "./UrlSearch.style";
 import { paths } from "../../router/routes";
-export const enterBattleText = "Enter new battle";
-export const enterUrl = "Enter Url or battle id:";
+import Pickahu from "./Pikachu";
+import { useNavigate } from "react-router-dom";
+
+const enterBattleText = "Enter new battle";
+const enterUrl = "Enter Url or battle id:";
 
 const UrlSearch = () => {
-  console.log("search box");
+  const navigate = useNavigate();
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    const battleRoomId = parseBattleIDFromUrl(e);
+    if (!battleRoomId) {
+      alert("sorry I could not find a battle for this url.");
+      return;
+    }
+    const path = paths.inBattleGenerator(battleRoomId);
+    navigate(path);
+  };
   return (
     <>
-      <UrlForm onSubmit={(e) => handleSubmit(e)}>
-        <UrlLabel htmlFor="url">{enterUrl}</UrlLabel>
-        <TextInput type="text" id="url" name="url" />
-        <SubmitInput type="submit" value="Submit" />
+      <UrlForm
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <Pickahu />
+        <h2>Connect to a Battle</h2>
+        <InputLabel htmlFor="url">
+          <TextInputField type="text" id="url" name="url" />
+          <InputLabelSpan>Enter Url or Battle Id Here</InputLabelSpan>
+        </InputLabel>
+        <ButtonContainer>
+          <Button type="submit" value="Submit">
+            Submit
+          </Button>
+        </ButtonContainer>
       </UrlForm>
     </>
   );
@@ -26,14 +58,6 @@ const parseBattleIDFromUrl = (e: React.SyntheticEvent) => {
   if (battleIndex === -1) {
     return null;
   }
-  // COULD ADD CHECK/PARSE FOR NON STANDARD CHARACTERS
   const battleRoomId = target.url.value.slice(battleIndex);
   return battleRoomId;
-};
-const handleSubmit = (e: React.SyntheticEvent) => {
-  e.preventDefault();
-  const battleRoomId = parseBattleIDFromUrl(e);
-  if (!battleRoomId) return;
-  const path = paths.inBattleGenerator(battleRoomId);
-  // ADD METHOD TO GO TO PATH
 };
