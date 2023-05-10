80e23f8a22b68746985633c19137a5a7870dc7d3
piacib <ben.piacitelli@gmail.com>
2023-05-09 17:43:42 -0700

    updated imports

--- src/components/EffectivenessQuiz/Results/Results.tsx
+++ src/components/EffectivenessQuiz/Results/Results.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Result } from "../EffectivenessQuiz";
+import { Result } from "../../../hooks/useQuiz/useQuiz";
 import { TypeBox } from "../../EffectivnessDisplay/EffectivnessDisplay.style";
 
 interface Props {
