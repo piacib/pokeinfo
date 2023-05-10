6dfe6af5c48949dfd43e8d4139903ff90cd3a9e8
piacib <ben.piacitelli@gmail.com>
2023-05-09 17:37:39 -0700

    set up search to go to new path

--- src/components/UrlSearch/UrlSearch.tsx
+++ src/components/UrlSearch/UrlSearch.tsx
@@ -1,30 +1,38 @@
-import { useState } from "react";
-import { BattleButton } from "../../App.style";
 import { TextInput, UrlForm, UrlLabel } from "./UrlSearch.style";
+import { paths } from "../../router/routes";
 export const enterBattleText = "Enter new battle";
 export const enterUrl = "Enter Url:";
-export const Form = ({ handleSubmit }: Props) => {
-  return (
-    <UrlForm onSubmit={(e) => handleSubmit(e)}>
-      <UrlLabel htmlFor="url">{enterUrl}</UrlLabel>
-      <TextInput type="text" id="url" name="url" />
-      <input type="submit" value="Submit" />
-    </UrlForm>
-  );
-};
-interface Props {
-  handleSubmit: (e: React.SyntheticEvent) => void;
-}
-const UrlSearch = ({ handleSubmit }: Props) => {
-  const [displayUrlInput, setDisplayUrlInput] = useState(false);
+
+const UrlSearch = () => {
   return (
     <>
-      <BattleButton onClick={() => setDisplayUrlInput(!displayUrlInput)}>
-        {enterBattleText}
-      </BattleButton>
-      {displayUrlInput && <Form handleSubmit={handleSubmit} />}
+      <UrlForm onSubmit={(e) => handleSubmit(e)}>
+        <UrlLabel htmlFor="url">{enterUrl}</UrlLabel>
+        <TextInput type="text" id="url" name="url" />
+        <input type="submit" value="Submit" />
+      </UrlForm>
     </>
   );
 };
 
 export default UrlSearch;
+
+const parseBattleIDFromUrl = (e: React.SyntheticEvent) => {
+  const target = e.target as typeof e.target & {
+    url: { value: string };
+  };
+  const battleIndex = target.url.value.indexOf("battle");
+  if (battleIndex === -1) {
+    return null;
+  }
+  // COULD ADD CHECK/PARSE FOR NON STANDARD CHARACTERS
+  const battleRoomId = target.url.value.slice(battleIndex);
+  return battleRoomId;
+};
+const handleSubmit = (e: React.SyntheticEvent) => {
+  e.preventDefault();
+  const battleRoomId = parseBattleIDFromUrl(e);
+  if (!battleRoomId) return;
+  const path = paths.inBattleGenerator(battleRoomId);
+  // ADD METHOD TO GO TO PATH
+};
