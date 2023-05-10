e9d8ba6e447745db23c93ded9356d8b725c165f8
piacib <ben.piacitelli@gmail.com>
2023-05-10 15:43:37 -0700

    added navigation redirect for extension

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { TypeWriterContainer } from "../../TypeWriterContainer.style";
 import {
   ButtonContainer,
@@ -7,10 +7,27 @@ import {
   WelcomeText,
   HomeLink,
 } from "./Home.style";
-import { Link } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { paths } from "../../router/routes";
-
-const Home: React.FC = ({}) => {
+// http://127.0.0.1:5173/pokeinfo/?battleId=battle-gen9randombattle-1861417829&isExtension=true
+const Home = () => {
+  // navigates to site if in extension
+  const navigate = useNavigate();
+  useEffect(() => {
+    const searchParams = window.location.search;
+    if (searchParams) {
+      const params = new URLSearchParams(searchParams);
+      const paramBattleId = params.get("battleId");
+      if (paramBattleId) {
+        params.delete("battleId");
+        const redirectPath = `${
+          paths.inBattle
+        }/${paramBattleId}/?${params.toString()}`;
+        console.log(redirectPath);
+        navigate(redirectPath);
+      }
+    }
+  }, []);
   return (
     <HomeContainer>
       <TypeWriterContainer>
@@ -21,7 +38,7 @@ const Home: React.FC = ({}) => {
         click example
       </WelcomeText>
       <ButtonContainer>
-        <HomeLink to={paths.search}>Search A battle</HomeLink>
+        <HomeLink to={paths.search}>Search A Battle</HomeLink>
         <HomeLink to={paths.example}>Example</HomeLink>
         <HomeLink to={paths.quiz}>Effectiveness Quiz</HomeLink>
       </ButtonContainer>
