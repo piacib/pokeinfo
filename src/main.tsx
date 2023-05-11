53194590af42da328fa0fb415c6358c57ee14d8a
piacib <ben.piacitelli@gmail.com>
2023-05-11 07:45:15 -0700

    updated imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  RouterProvider,
-} from "react-router-dom";
-import { GlobalStyles } from "./GlobalStyles";
+import { RouterProvider } from "react-router-dom";
+import { GlobalStyle } from "./styles/GlobalStyles";
 import { ThemeProvider } from "styled-components";
-import { themeObjGenerator } from "./theme";
+import { themeObjGenerator } from "./styles/theme";
 import router from "./router/browserRouter";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <GlobalStyles theme={themeObjGenerator("light")} />
+    <GlobalStyle theme={themeObjGenerator("light")} />
     <ThemeProvider theme={themeObjGenerator("light")}>
       <RouterProvider router={router} />
     </ThemeProvider>
