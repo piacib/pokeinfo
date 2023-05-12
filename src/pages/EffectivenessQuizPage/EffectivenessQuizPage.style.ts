e7abf5b4a5b039eb32dea86b2bdba0fbfc8984aa
piacib <ben.piacitelli@gmail.com>
2023-05-12 06:25:15 -0700

    added flex direction

--- src/pages/EffectivenessQuizPage/EffectivenessQuizPage.style.ts
+++ src/pages/EffectivenessQuizPage/EffectivenessQuizPage.style.ts
@@ -1,3 +1,5 @@
 import styled from "styled-components";
 import { ContentCenteredContainer } from "../Page.style";
-export const Container = styled(ContentCenteredContainer)``;
+export const Container = styled(ContentCenteredContainer)`
+  flex-direction: column;
+`;
