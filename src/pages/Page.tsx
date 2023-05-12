f2140ec2f08c3a06c36af707df0e61d9e9bb5b45
piacib <ben.piacitelli@gmail.com>
2023-05-12 14:07:48 -0700

    added header prop

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { Header } from "../App.style";
 import OptionsMenu from "../components/OptionsMenu/OptionsMenu";
+import { GlobalStyle } from "../GlobalStyles";
+import { themeObjGenerator } from "../styles/theme";
 
 const Page = ({
   children,
+  header = false,
 }: {
+  header?: boolean;
   children?: React.ReactNode[] | Element | JSX.Element;
 }) => {
   return (
     <>
-      <Header>
-        <OptionsMenu />
-      </Header>
+      <GlobalStyle />
+      {header && (
+        <Header>
+          <OptionsMenu />
+        </Header>
+      )}
       {children}
     </>
   );
