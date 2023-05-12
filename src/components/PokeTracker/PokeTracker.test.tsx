bc32eb4f30eaa7c10902e19ef16af14d0575ea9c
piacib <ben.piacitelli@gmail.com>
2023-05-12 07:58:21 -0700

    added test

---
+++ src/components/PokeTracker/PokeTracker.test.tsx
@@ -0,0 +1,41 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { vi } from "vitest";
+
+import PokeTracker, { stopText, trackText } from "./PokeTracker";
+// Tests that clicking the toggle button toggles the state and displays the correct text.
+it("test_toggle_button_toggles_state", () => {
+  let mockToggle = false;
+  const mockHandleChange = vi.fn();
+  const mockSetToggle = vi.fn(() => (mockToggle = !mockToggle));
+  render(
+    <PokeTracker
+      toggle={mockToggle}
+      setToggle={mockSetToggle}
+      handleChange={mockHandleChange}
+    />,
+  );
+
+  const button = screen.getByRole("button");
+  expect(button).toHaveTextContent(trackText);
+  fireEvent(
+    button,
+    new MouseEvent("click", {
+      bubbles: true,
+      cancelable: true,
+    }),
+  );
+  expect(mockHandleChange).toHaveBeenCalled();
+  expect(mockSetToggle).toHaveBeenCalled();
+  expect(mockToggle).toBeTruthy();
+  fireEvent(
+    button,
+    new MouseEvent("click", {
+      bubbles: true,
+      cancelable: true,
+    }),
+  );
+  screen.debug();
+  expect(mockHandleChange).toHaveBeenCalledTimes(2);
+  expect(mockSetToggle).toHaveBeenCalled();
+  expect(mockToggle).toBeFalsy();
+});
--- src/components/PokeTracker/PokeTracker.tsx
+++ src/components/PokeTracker/PokeTracker.tsx
@@ -1,36 +1,24 @@
 import React from "react";
-import {
-  ToggleCheckbox,
-  ToggleContainer,
-  ToggleLabel,
-  ToggleLabelSpan,
-} from "./PokeTracker.styled";
+import { ToggleButton } from "./PokeTracker.styled";
 interface Props {
   toggle: boolean;
   setToggle: React.Dispatch<React.SetStateAction<boolean>>;
-  handleChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleChange?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
+export const trackText = "Track Active Pokemon";
+export const stopText = "Stop Tracking Active";
 const PokeTracker = ({ handleChange, toggle, setToggle }: Props) => {
   return (
-    <>
-      <h3>{!toggle ? "Track Active Pokemon" : "Stop Tracking Active"}:</h3>
-      <ToggleContainer>
-        <ToggleCheckbox
-          type="checkbox"
-          id="activePkmToggle"
-          onChange={(e) => {
-            if (handleChange) {
-              handleChange(e);
-            }
-            setToggle(!toggle);
-          }}
-          checked={toggle}
-        />
-        <ToggleLabel htmlFor="activePkmToggle">
-          <ToggleLabelSpan></ToggleLabelSpan>
-        </ToggleLabel>
-      </ToggleContainer>
-    </>
+    <ToggleButton
+      onClick={(e) => {
+        if (handleChange) {
+          handleChange(e);
+        }
+        setToggle(!toggle);
+      }}
+    >
+      {!toggle ? trackText : stopText}
+    </ToggleButton>
   );
 };
 
