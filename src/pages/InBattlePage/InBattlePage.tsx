c24a927de930009239e7696bc9852c0f7113d7a4
piacib <ben.piacitelli@gmail.com>
2023-05-12 14:25:43 -0700

    moved extension logic to page

--- src/pages/InBattlePage/InBattlePage.tsx
+++ src/pages/InBattlePage/InBattlePage.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Page from "../Page";
 import { InBattleDisplay } from "../../components/InBattleDisplay/InBattleDisplay";
+import { Spacer } from "../../App.style";
 
 const InBattlePage = () => {
+  const params = new URLSearchParams(window.location.search);
+  const isExtension = params.has("isExtension");
   return (
-    <Page>
+    <Page header={!isExtension}>
+       {isExtension && <Spacer />}
       <InBattleDisplay />
     </Page>
   );
