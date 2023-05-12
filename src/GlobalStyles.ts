c4b4bcf0a06891a657fc89c13b4010e95a0ed939
piacib <ben.piacitelli@gmail.com>
2023-05-12 14:06:58 -0700

    removed unused styles

--- src/GlobalStyles.ts
+++ src/GlobalStyles.ts
@@ -19,11 +19,11 @@ export const GlobalStyle = createGlobalStyle`
 }
 html{
     /* font-size:${isDevelopmentMode ? "10px" : ""}; */
-    background-color: ${(props) => props.theme.backgroundColor};
+    background-color:#eef2f5;
   }
   body {
   overscroll-behavior:contain;
-    background-color: ${(props) => props.theme.backgroundColor};
+    background-color:#eef2f5;
     margin: 0;
   display: flex;
   min-height: 100vh;
@@ -32,32 +32,5 @@ html{
   display: flex;
   flex-direction: column;
   width: 100%;
-}
-button {
-  border-radius: 8px;
-  border: 1px solid transparent;
-  padding: 0.6em 1.2em;
-  font-size: 1em;
-  font-weight: 500;
-  font-family: inherit;
-  background-color: #f9f9f9;
-  cursor: pointer;
-  transition: border-color 0.25s;
-}
-button:hover {
-  border-color: #646cff;
-}
-button:focus,
-button:focus-visible {
-  outline: 4px auto -webkit-focus-ring-color;
-}
-h1 {
-  font-size: 3.2em;
-  line-height: 1.1;
-  color: ${(props) => props.theme.fontColor};
-}
-form,input {
-  color: ${(props) => props.theme.fontColor};
-
 }
 `;
