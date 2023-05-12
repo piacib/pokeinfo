a1f0aa1b101687dc3004572265673f08eebcd0ac
piacib <ben.piacitelli@gmail.com>
2023-05-12 14:23:24 -0700

    added redirect

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { TypeWriterContainer } from "../../styles/TypeWriterContainer.style";
 import {
   ButtonContainer,
@@ -8,8 +8,21 @@ import {
   HomeLink,
 } from "./Home.style";
 import { paths } from "../../router/routes";
+import { useNavigate } from "react-router-dom";
 
 const Home: React.FC = ({}) => {
+  const navigate = useNavigate();
+  const params = new URLSearchParams(window.location.search);
+  const paramBattleId = params.get("battleId");
+  useEffect(() => {
+    if (paramBattleId) {
+      console.log(paramBattleId);
+      const path = paths.inBattleGenerator(paramBattleId);
+      console.log(path);
+      console.log(params);
+      navigate(path);
+    }
+  });
   return (
     <HomeContainer>
       <TypeWriterContainer>
