89d889d0ae14428261c33bb8c8d29f8d1a30aa65
piacib <ben.piacitelli@gmail.com>
2023-05-12 06:24:28 -0700

    added style

---
+++ src/components/EffectivenessQuiz/Results/Results.style.ts
@@ -0,0 +1,48 @@
+import styled, { css } from "styled-components";
+import { TypeBox } from "../../EffectivnessDisplay/EffectivnessDisplay.style";
+import { PillDesign } from "../../../App.style";
+export const ResultRow = styled.tr``;
+const fontCss = css`
+  font-size: 0.8rem;
+  @media screen and (min-width: ${(props) => props.theme.media.mediumScreen}) {
+    font-size: 1.2rem;
+  }
+`;
+export const ResutTable = styled.table`
+  border-spacing: 0.5rem;
+  margin: 0 1rem;
+  ${fontCss}
+`;
+export const ResultCell = styled.td``;
+interface Props {
+  correct?: boolean;
+}
+export const AnswerContainer = styled.div`
+  width: 100%;
+  justify-content: flex-start;
+  display: flex;
+  align-items: flex-start;
+  align-items: flex-start;
+  padding: 0 1rem;
+`;
+export const ChoiceContainer = styled.div<Props>`
+  width: 100%;
+  justify-content: center;
+  background: ${(props) => (props.correct ? "green" : "red")};
+  ${PillDesign}
+  display: flex;
+  align-items: center;
+`;
+export const PokemonTypeCell = styled.div`
+  justify-content: center;
+  max-width: 15rem;
+  flex-wrap: wrap;
+  display: flex;
+  align-items: center;
+`;
+export const ResultTypedBox = styled(TypeBox)`
+  display: grid;
+  place-items: center;
+  ${fontCss}
+`;
+export const TableColumn = styled.th``;
