7ba56c85e8fad20619dc4582f2ceea72b98f3c80
piacib <ben.piacitelli@gmail.com>
2023-05-12 14:26:51 -0700

    moved redirect logic to page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Page from "../Page";
 import Home from "../../components/Home/Home";
+import { useNavigate } from "react-router-dom";
+import { paths } from "../../router/routes";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+  const params = new URLSearchParams(window.location.search);
+  const paramBattleId = params.get("battleId");
+  useEffect(() => {
+    if (paramBattleId) {
+      console.log(paramBattleId);
+      const path = paths.inBattleGenerator(paramBattleId);
+      console.log(path);
+      console.log(params);
+      navigate(path);
+    }
+  });
   return (
     <Page>
       <Home />
