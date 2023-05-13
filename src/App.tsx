331c54d80c91013fff0ba896d664049fc4c0f645
piacib <ben.piacitelli@gmail.com>
2023-05-13 08:09:50 -0700

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import { GlobalStyle } from "./GlobalStyles";
 import { ThemeProvider } from "styled-components";
 import { RouterProvider } from "react-router-dom";
 import { themeObjGenerator } from "./styles/theme";
-import router from "./router/browserRouter";
+import router from "./router/hashRouter";
 import OptionsMenu from "./components/OptionsMenu/OptionsMenu";
 import { Header } from "./App.style";
 
 const App = () => (
   <>
     <ThemeProvider theme={themeObjGenerator("light")}>
-  <GlobalStyle/>
-      <RouterProvider router={router}/>
+      <GlobalStyle />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </>
 );
