0aab8ec809ee93d5e5d808fffae6d82afded25bf
piacib <ben.piacitelli@gmail.com>
2023-05-13 08:10:18 -0700

    removed unused imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { GlobalStyle } from "./GlobalStyles";
-import { ThemeProvider } from "styled-components";
-import { themeObjGenerator } from "./styles/theme";
-import router from "./router/browserRouter";
+
 import App from "./App";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
