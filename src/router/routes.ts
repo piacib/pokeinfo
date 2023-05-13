9ec9b12fec68c74d7e6626ce294729ce3c798ff1
piacib <ben.piacitelli@gmail.com>
2023-05-13 08:08:02 -0700

    added extension route

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,6 +5,9 @@ export const paths = {
   inBattle: "/inBattle/:id",
   search: "/search",
   inBattleGenerator: (battleId: string) => `/inBattle/${battleId}`,
+  extension: "/extension/inBattle/:id",
+  inExtensionGenerator: (battleId: string) => `/extension/inBattle/${battleId}`,
+
   quiz: "/quiz",
   home: "/home",
   example: `/inBattle/${devRoomId}`,
