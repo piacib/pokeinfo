fd2233ef4b618ba1448c3d3bc8500d390a491ccf
piacib <ben.piacitelli@gmail.com>
2023-05-13 08:10:31 -0700

    removed unused route

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,13 +1,11 @@
 import { devRoomId } from "../developmentMode";
 
 export const paths = {
-  effectiveness: "/effectiveness",
   inBattle: "/inBattle/:id",
   search: "/search",
   inBattleGenerator: (battleId: string) => `/inBattle/${battleId}`,
   extension: "/extension/inBattle/:id",
   inExtensionGenerator: (battleId: string) => `/extension/inBattle/${battleId}`,
-
   quiz: "/quiz",
   home: "/home",
   example: `/inBattle/${devRoomId}`,
