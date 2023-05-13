4919ac9d5fd96e03fb23def5c4f6a5951cbae419
piacib <ben.piacitelli@gmail.com>
2023-05-13 08:07:13 -0700

    added hook to redirect

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,20 +3,23 @@ import Page from "../Page";
 import Home from "../../components/Home/Home";
 import { useNavigate } from "react-router-dom";
 import { paths } from "../../router/routes";
-
-const HomePage = () => {
+const useExtensionRedirect = () => {
   const navigate = useNavigate();
   const params = new URLSearchParams(window.location.search);
   const paramBattleId = params.get("battleId");
   useEffect(() => {
     if (paramBattleId) {
       console.log(paramBattleId);
-      const path = paths.inBattleGenerator(paramBattleId);
+      const path = paths.inExtensionGenerator(paramBattleId);
       console.log(path);
       console.log(params);
       navigate(path);
     }
   });
+};
+
+const HomePage = () => {
+  useExtensionRedirect();
   return (
     <Page>
       <Home />
