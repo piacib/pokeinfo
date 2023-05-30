d0d0b4c2962c138483a2cde651cd3268637963e8
piacib <ben.piacitelli@gmail.com>
2023-05-30 11:34:22 -0700

    added styled components for most used styles

---
+++ src/styles/Components.style.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const FlexDiv = styled.div`
+  display: flex;
+`;
+export const FlexRowDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+export const FlexColumnDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const FlexRowCenteredDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+export const FlexColumnCenteredDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
