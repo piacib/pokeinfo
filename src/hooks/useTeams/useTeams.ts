d76615d0367a4a9180101199a2f15a8ccd40e07b
piacib <ben.piacitelli@gmail.com>
2023-06-12 12:39:20 -0700

    added hook to toggle between nospectator and ws

---
+++ src/hooks/useTeams/useTeams.ts
@@ -0,0 +1,26 @@
+import { useParams } from "react-router-dom";
+import useNoSpectator from "../useNoSpectators/useNoSpectator";
+import { teamsType, useWebSocket } from "../useWebsSocket/useWebsSocket";
+import { useRef } from "react";
+type TeamData = {
+  teams: teamsType;
+  setTeams: React.Dispatch<React.SetStateAction<teamsType>>;
+  activePokemon: teamsType;
+};
+
+export interface TeamsReturn extends TeamData {
+  noSpectators: boolean;
+}
+const useTeams = (): TeamsReturn => {
+  const previousBattleRoomId = useRef("");
+
+  const { id } = useParams();
+  const websocketData = useWebSocket(`${id}`, previousBattleRoomId.current);
+  const noSpectatorData = useNoSpectator();
+
+  if (noSpectatorData === false) {
+    return { ...websocketData };
+  }
+  return { ...noSpectatorData };
+};
+export default useTeams;
