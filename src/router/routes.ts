3ca6bf46466e7b53da2a7ecfe4009cd246bebd14
piacib <ben.piacitelli@gmail.com>
2023-06-12 14:59:16 -0700

    added pokemonsearch route

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,6 +3,7 @@ import { devRoomId } from "../developmentMode";
 export const paths = {
   inBattle: "/inBattle/:id",
   search: "/search",
+  pokemonsearch: "/pokemonsearch",
   inBattleGenerator: (battleId: string) => `/inBattle/${battleId}`,
   extension: "/extension/inBattle/:id",
   inExtensionGenerator: (battleId: string) => `/extension/inBattle/${battleId}`,
