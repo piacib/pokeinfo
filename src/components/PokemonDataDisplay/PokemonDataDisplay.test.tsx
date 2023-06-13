859351171eae7477e36b816aaef56470c10cc7ca
piacib <ben.piacitelli@gmail.com>
2023-06-13 07:12:47 -0700

    updated test to utilizes useParams

--- src/components/PokemonDataDisplay/PokemonDataDisplay.test.tsx
+++ src/components/PokemonDataDisplay/PokemonDataDisplay.test.tsx
@@ -1,27 +1,30 @@
 import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../../styles/theme";
-import "@testing-library/jest-dom";
 import PokemonDataDisplay from "./PokemonDataDisplay";
-const handleSubmit = (e: React.SyntheticEvent): void => {
-  return;
-};
+import { vi } from "vitest";
+import { Params } from "react-router-dom";
+vi.mock("react-router-dom", () => ({
+  useParams: (): Readonly<Params<string>> => ({ id: "battle-gen9ou-" }),
+}));
+
 describe("PokemonDataDisplay", () => {
   const pokemon = "Azumarill";
   it("Pokemon name appears", () => {
     render(
       <ThemeProvider theme={theme}>
-        <PokemonDataDisplay pokemon={pokemon} battleType="gen9ou" />
+        <PokemonDataDisplay pokemon={pokemon} />
       </ThemeProvider>,
     );
+    screen.debug();
     const PokemonName = screen.getByText(pokemon);
     expect(PokemonName).toBeInTheDocument();
   });
   it("Pokemon stats appears", () => {
     render(
       <ThemeProvider theme={theme}>
-        <PokemonDataDisplay pokemon={pokemon} battleType="gen9ou" />
+        <PokemonDataDisplay pokemon={pokemon} />
       </ThemeProvider>,
     );
     const stats = screen.getByText(new RegExp("stats", "i"));
@@ -34,7 +37,7 @@ describe("PokemonDataDisplay", () => {
   it("Incorrect Pokemon cause error screen", () => {
     render(
       <ThemeProvider theme={theme}>
-        <PokemonDataDisplay pokemon={"badName"} battleType="gen9ou" />
+        <PokemonDataDisplay pokemon={"badName"} />
       </ThemeProvider>,
     );
     const errorMessage = screen.getByText("Enter Pokemon:");
