41be85670ae6626da07ce1dced0842ebbd9efcf7
piacib <ben.piacitelli@gmail.com>
2023-06-19 06:54:34 -0700

    removed unused imports

--- src/components/AbilitiesDisplay/abilities.test.tsx
+++ src/components/AbilitiesDisplay/abilities.test.tsx
@@ -1,10 +1,8 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../../styles/theme";
 import AbilitiesDisplay from "./AbilitiesDisplay";
 import "@testing-library/jest-dom";
-import { testAbilities } from "../../developmentMode";
 test("Abilities text is present", () => {
   render(
     <ThemeProvider theme={theme}>
