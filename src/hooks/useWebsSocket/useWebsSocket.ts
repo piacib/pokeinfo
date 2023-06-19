6b5bc84334393eee97f14b3845d5df62f098399c
piacib <ben.piacitelli@gmail.com>
2023-06-19 07:15:56 -0700

    removed unused imports

--- src/hooks/useWebsSocket/useWebsSocket.ts
+++ src/hooks/useWebsSocket/useWebsSocket.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer, useRef, useState } from "react";
+import React, { useEffect,  useRef, useState } from "react";
 import { isRandomBattle } from "../../functions";
 import {
   getActivePokemon,
