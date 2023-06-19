08dcc13616cd531958afa1fca9bcf1607d2dc050
piacib <ben.piacitelli@gmail.com>
2023-06-19 07:13:19 -0700

    removed unused imports

--- src/components/PokemonDataDisplay/DataDisplay.style.ts
+++ src/components/PokemonDataDisplay/DataDisplay.style.ts
@@ -1,5 +1,4 @@
 import styled, { css } from "styled-components";
-import { FlexColumnDiv } from "../../styles/Components.style";
 
 export const PropertyDisplay = styled.div`
   height: fit-content;
